9d7747bb1f4e4564a006d3f170fc79929cbbba64
arifreyrg <arifreyr@gmail.com>
2016-02-11 14:33:00 +0000

    More fine changes

---
+++ app/src/main/java/is/hi/hbv2/tvwatch/DisplayChannelFragment.java
@@ -0,0 +1,16 @@
+package is.hi.hbv2.tvwatch;
+
+import android.support.v4.app.Fragment;
+
+import java.util.ArrayList;
+
+import is.hi.hbv2.tvwatch.ItemHolster.BroadCastItem;
+
+/**
+ * Created by ari on 10-Feb-16.
+ */
+public class DisplayChannelFragment extends Fragment {
+    private ArrayList<BroadCastItem> sched;
+
+
+}
--- app/src/main/java/is/hi/hbv2/tvwatch/MainActivity.java
+++ app/src/main/java/is/hi/hbv2/tvwatch/MainActivity.java
@@ -1,12 +1,16 @@
 package is.hi.hbv2.tvwatch;
 
 
+import android.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
 
-public class MainActivity extends FragmentActivity {
+public class MainActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,10 +19,24 @@ public class MainActivity extends FragmentActivity {
 
         FragmentManager fm = getSupportFragmentManager();
         Fragment f = fm.findFragmentById(R.id.container);
-
+        /*
         if(f == null){
             f = new MainMenuFragment();
             fm.beginTransaction().add(R.id.container, f).commit();
         }
+        */
+
+        // get action bar
+        ActionBar actionBar = getActionBar();
+
+        // Enabling Up / Back navigation
+        //actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.activity_main_actions, menu);
+
+        return super.onCreateOptionsMenu(menu);
     }
 }
