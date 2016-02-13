3049f67456195e7655e94b23739a7b44a0e42201
arifreyrg <arifreyr@gmail.com>
2016-02-13 16:49:53 +0000

    more more

--- app/src/main/java/is/hi/hbv2/tvwatch/MainActivity.java
+++ app/src/main/java/is/hi/hbv2/tvwatch/MainActivity.java
@@ -5,7 +5,7 @@ import android.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
-import android.os.Bundle;
+import android.os.Bundle; 
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
