fbf6d9388809e8ecdfa4272840e3e51ede6536f2
arifreyrg <arifreyr@gmail.com>
2016-03-05 16:34:56 +0000

    superString + libs

--- app/src/main/java/is/hi/hbv2/tvwatch/Textavarpid.java
+++ app/src/main/java/is/hi/hbv2/tvwatch/Textavarpid.java
@@ -1,5 +1,7 @@
 package is.hi.hbv2.tvwatch;
 
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 
 /**
@@ -9,7 +11,15 @@ public class Textavarpid {
 
 
     //API til að fá formattaða lista
+    String superString = "{results:[{title:KrakkaRÚV,originalTitle:,duration:02:55:00,description:,"+
+        "shortDescription:,live:false,premier:true,startTime:2016-03-05 07:00:00,aspectRatio:16:9,"+
+            "series:{episode:1,series:1}},{title:Háværa ljónið Urri,originalTitle:Raa Raa,duration:00:10:04,"+
+            "description:,shortDescription:,live:false,premier:true,startTime:2016-03-05 07:01:00,"+
+            "aspectRatio:16:9,series:{episode:47,series:52}},{title:Nellý og Nóra,originalTitle:Nelly & Nora,"+
+            "duration:00:07:15,description:,shortDescription:,live:false,premier:true,startTime:2016-03-05 07:11:00,"+
+            "aspectRatio:16:9,series:{episode:14,series:52}},]}";
 
+    JSONObject json = new JSONObject(superString);
 
     public static ArrayList<BroadCastItem> getFavorites(){
         return populateSched();
