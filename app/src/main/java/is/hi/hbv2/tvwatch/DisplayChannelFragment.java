f558e3a406b1e5fc438fc6eb7a9a40a016a8204b
Þorvaldur Rúnarsson <valdirunars@gmail.com>
2016-04-16 15:37:21 +0000

    Dialog finished

--- app/src/main/java/is/hi/hbv2/tvwatch/DisplayChannelFragment.java
+++ app/src/main/java/is/hi/hbv2/tvwatch/DisplayChannelFragment.java
@@ -86,15 +86,34 @@ public class DisplayChannelFragment extends Fragment implements JSONFetching {
     @Override
     public void didFailToFetch() {
         Log.d("Internet connection failure!","Internet is Down cannot fetch data");
-        AlertDialog alertDialog = new AlertDialog.Builder(this.getActivity()).create();
-        alertDialog.setTitle("Alert");
-        alertDialog.setMessage("Alert message to be shown");
+        final MainActivity activity = (MainActivity) getActivity();
+        activity.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                mDialog.dismiss();
+            }
+        });
+        if (activity.isAlertRunning)
+        {
+            return;
+        }
+        AlertDialog alertDialog = new AlertDialog.Builder(activity).create();
+        alertDialog.setTitle("No Internet Connection");
+        alertDialog.setMessage("Please establish a solid internet connection and try again.");
+
         alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+
                 new DialogInterface.OnClickListener() {
+
                     public void onClick(DialogInterface dialog, int which) {
                         dialog.dismiss();
+                        dialog.cancel();
+                        activity.isAlertRunning = false;
+
                     }
+
                 });
         alertDialog.show();
+        activity.isAlertRunning = true;
     }
 }
